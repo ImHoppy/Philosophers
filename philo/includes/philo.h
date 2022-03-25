00e81688cd379d22e4208cadbaf53c0943eccc7f
Marius BRAETS <mbraets@student.42.fr>
2022-03-25 13:08:49 +0100

    Finish parse

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,12 +6,44 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 13:44:31 by mbraets           #+#    #+#             */
-/*   Updated: 2022/03/24 13:46:33 by mbraets          ###   ########.fr       */
+/*   Updated: 2022/03/25 13:06:20 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+# define LOG_TAKEN_FORK	= "%d %d has taken a fork\n"
+# define LOG_EATING		= "%d %d is eating\n"
+# define LOG_SLEEPING	= "%d %d is sleeping\n"
+# define LOG_THINKING	= "%d %d is thinking\n"
+# define LOG_DIE		= "%d %d died\n"
+
+typedef struct s_philo {
+	pthread_mutex_t	*left;
+	pthread_mutex_t	*right;
+	int				index;
+}	t_philo;
+
+typedef struct s_data {
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+	struct timeval	start_time;
+	unsigned int	philo_max;
+	unsigned int	time_die;
+	unsigned int	time_eat;
+	unsigned int	time_sleep;
+	unsigned int	eat_max;
+}	t_data;
+
+int	ft_atoi(const char *str);
 
 #endif
 
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 13:44:28 by mbraets           #+#    #+#             */
-/*   Updated: 2022/03/25 12:37:11 by mbraets          ###   ########.fr       */
+/*   Updated: 2022/03/25 13:08:07 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 int	philo_parse(t_data *data, int ac, char **av)
 {
-	if (ac != 5 | ac != 6)
+	if ((ac != 5) & (ac != 6))
 		return (1);
-	if (ac == 6)
-		data->eat_max = ft_atoi(av[5]);
 	*data = (t_data){.philo_max = ft_atoi(av[1]), .time_die = ft_atoi(av[2]),
 		.time_eat = ft_atoi(av[3]), .time_sleep = ft_atoi(av[4]),
-		.eat_max = (*data).eat_max};
+		.eat_max = ft_atoi(av[5])};
 
 	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	// t_data	data;
+	t_data	data;
 
-	printf("%d\n", ft_atoi("f1"));
-	// if (philo_parse(&data, ac, av))
-		// return (1);
+	if (philo_parse(&data, ac, av) == 1)
+		return (write(2, "Error\n", 6), 1);
+	gettimeofday(&data.start_time, NULL);
 	(void) ac;
 	(void) av;
 	return (0);
