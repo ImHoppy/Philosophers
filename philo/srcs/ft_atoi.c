589121d1302b98557703f768cb2fb37c76ce61d6
Marius BRAETS <mbraets@student.42.fr>
2022-03-25 12:37:41 +0100

    Add atoi and fix segFault on null input

---
+++ philo/srcs/ft_atoi.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 15:23:32 by mbraets           #+#    #+#             */
+/*   Updated: 2022/03/25 12:36:34 by mbraets          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+static int	ft_isspace(int c)
+{
+	if ((c >= 9 && c <= 13) || c == ' ')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		negative;
+	long	result;
+	int		i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	result = 0;
+	negative = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		negative = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] != '\0' && ft_isdigit(str[i]))
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result > 2147483648 && negative == -1)
+			return (0);
+		if (result > 2147483647 && negative == 1)
+			return (-1);
+		i++;
+	}
+	return (result * negative);
+}
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,14 +6,33 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 13:44:28 by mbraets           #+#    #+#             */
-/*   Updated: 2022/03/24 13:46:14 by mbraets          ###   ########.fr       */
+/*   Updated: 2022/03/25 12:37:11 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+int	philo_parse(t_data *data, int ac, char **av)
+{
+	if (ac != 5 | ac != 6)
+		return (1);
+	if (ac == 6)
+		data->eat_max = ft_atoi(av[5]);
+	*data = (t_data){.philo_max = ft_atoi(av[1]), .time_die = ft_atoi(av[2]),
+		.time_eat = ft_atoi(av[3]), .time_sleep = ft_atoi(av[4]),
+		.eat_max = (*data).eat_max};
+
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
+	// t_data	data;
+
+	printf("%d\n", ft_atoi("f1"));
+	// if (philo_parse(&data, ac, av))
+		// return (1);
 	(void) ac;
 	(void) av;
-	
 	return (0);
 }
